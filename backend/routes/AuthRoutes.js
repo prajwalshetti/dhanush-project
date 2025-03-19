51e2acd2f05544dace50f08a22dd6fd02f4b220b
dhanushc8264 <dhanush.chidanand@gmail.com>
2025-03-20 02:26:45 +0530

    updated updatedonationstatus and requestdonationstatus in both frontend and backend

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import protectedRoute from '../middlewares/AuthMiddleware.js'
 import { registerController , loginController } from '../controllers/AuthController.js'
+import User from '../models/userModel.js'
 
 
 const router = express.Router()
@@ -13,6 +14,18 @@ router.get('/profile', protectedRoute, (req, res) => {
     res.json({ success: true, message: "User authenticated", user: req.user });
 });
 
+router.get('/getUserById/:id', protectedRoute, async (req, res) => {
+  try {
+      const userId = req.params.id;
+      const user = await User.findById(userId); // Adjust this line based on your User model implementation
+      if (!user) {
+          return res.status(404).json({ success: false, message: 'User not found' });
+      }
+      res.status(200).json({ success: true, user });
+  } catch (error) {
+      res.status(500).json({ success: false, message: 'Server error', error: error.message });
+  }
+});
 router.get('/check', protectedRoute, (req, res) => {
     res.status(200).json({ success: true, user: req.user })
   })
